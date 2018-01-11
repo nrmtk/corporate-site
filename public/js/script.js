6323cb889beaa45efff59326e1ae9e929eb2f3c0
DESKTOP-GP5S5LS\user <nrm.takezaki@gmail.com>
2018-01-11 17:23:49 +0900

    contactページ完成

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,15 @@
 $(function(){
-    
-})
+    $(".toggle-button").click(function(){
+      $(".nav-toggle-list").slideToggle(800);
+      $(".toggle-button").hide();
+      $(".toggle-close").show();
+    });
+});
+
+$(function(){
+    $(".toggle-close").click(function(){
+      $(".nav-toggle-list").slideToggle(800);
+      $(".toggle-close").hide();
+      $(".toggle-button").show();
+    });
+});
